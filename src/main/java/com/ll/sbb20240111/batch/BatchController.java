c510b3b86072ccb6cee36fa334a9bb624aa3e0de
seo-sang-ho <rocter2049@naver.com>
2024-01-15 10:34:26 +0900

    mission

---
+++ src/main/java/com/ll/sbb20240111/batch/BatchController.java
@@ -0,0 +1,33 @@
+package com.ll.sbb20240111.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/batch")
+@RequiredArgsConstructor
+public class BatchController {
+    private final BatchService batchService;
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String runSimpleJob() {
+        batchService.runHelloJob();
+
+        return "runHelloJob OK";
+    }
+}
--- src/main/java/com/ll/sbb20240111/batch/HelloJobConfig.java
+++ src/main/java/com/ll/sbb20240111/batch/HelloJobConfig.java
@@ -3,7 +3,10 @@ package com.ll.sbb20240111.batch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -16,12 +19,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class HelloJobConfig {
     @Bean
-    public Job helloJob(JobRepository jobRepository, Step simpleStep1) {
+    public Job helloJob(JobRepository jobRepository, Step helloStep1) {
         return new JobBuilder("helloJob", jobRepository)
-                .start(simpleStep1)
+                .start(helloStep1)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
+    @JobScope
     @Bean
     public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet1, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("helloStep1Tasklet1", jobRepository)
@@ -29,11 +34,12 @@ public class HelloJobConfig {
                 .build();
     }
 
+    @StepScope
     @Bean
     public Tasklet helloStep1Tasklet1() {
         return ((contribution, chunkContext) -> {
             log.info("Hello World");
-            System.out.println("Hello World");
+            System.out.println("Hello World 1/1");
             return RepeatStatus.FINISHED;
         });
     }
