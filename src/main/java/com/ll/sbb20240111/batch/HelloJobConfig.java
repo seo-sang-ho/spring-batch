9eeecbd378bcaa0a8a04e72a34724fcc7f4588ad
seo-sang-ho <rocter2049@naver.com>
2024-01-15 09:31:00 +0900

    feat: print hello-world use spring batch

--- src/main/java/com/ll/sbb20240111/batch/HelloJobConfig.java
+++ src/main/java/com/ll/sbb20240111/batch/HelloJobConfig.java
@@ -1,36 +1,38 @@
-package com.ll.sbb20240111;
+package com.ll.sbb20240111.batch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
-public class HelloConfiguration {
+public class HelloJobConfig {
     @Bean
-    public Job job(JobRepository jobRepository, Step step1) {
-        return new JobBuilder("job", jobRepository)
-                .start(step1)
+    public Job helloJob(JobRepository jobRepository, Step simpleStep1) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(simpleStep1)
                 .build();
     }
+
     @Bean
-    public Step step(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
-        return new StepBuilder("step", jobRepository)
-                .tasklet(testTasklet, platformTransactionManager).build();
+    public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet1, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloStep1Tasklet1", jobRepository)
+                .tasklet(helloStep1Tasklet1, platformTransactionManager)
+                .build();
     }
+
     @Bean
-    public Tasklet testTasklet(){
+    public Tasklet helloStep1Tasklet1() {
         return ((contribution, chunkContext) -> {
+            log.info("Hello World");
             System.out.println("Hello World");
             return RepeatStatus.FINISHED;
         });
